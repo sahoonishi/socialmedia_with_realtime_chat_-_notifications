dfd261c0dafc70e8f4ad811552c902084d44aaf1
sahoonishi <nishisahoo293@gmail.com>
2025-02-16 23:50:31 +0530

    mongoDB connected

---
+++ backend/server.js
@@ -0,0 +1,30 @@
+import express,{urlencoded} from "express";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+import dotenv from "dotenv";
+import connectDB from "./utils/db.js";
+dotenv.config({});
+
+const app = express();
+const port = process.env.PORT || 8000;
+
+// middlewares 
+app.use(express.json());
+app.use(cookieParser());
+app.use(urlencoded({extended:true}));
+const corsOptions = {
+  origin:"http://localhost:3000",
+  credentials:true
+}
+app.use(cors(corsOptions));
+
+app.listen(port , ()=>{
+  connectDB();
+  // console.log(`server running at ${port}`);
+})
+app.get("/",(_,res)=>{
+  return res.status(200).json({
+    message:"I am backend , Express",
+    success:true
+  })
+})
