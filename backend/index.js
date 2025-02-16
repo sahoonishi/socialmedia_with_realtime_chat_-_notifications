5d10e49f47127bc36caf0e48509347925fdfadf8
sahoonishi <nishisahoo293@gmail.com>
2025-02-16 22:50:01 +0530

    first commit

---
+++ backend/index.js
@@ -0,0 +1,12 @@
+import express from "express";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+
+const app = express();
+const port = 8080;
+
+// middlewares 
+
+app.listen(Port , ()=>{
+  console.log(`server running at ${port}`)
+})
