67a592220ca054819dd62293c406dbc7e23db71e
sahoonishi <nishisahoo293@gmail.com>
2025-03-28 21:21:23 +0530

    message window

--- frontend/src/components/Chat.jsx
+++ frontend/src/components/Chat.jsx
@@ -3,6 +3,9 @@ import Layout from "./Layout";
 import { useDispatch, useSelector } from "react-redux";
 import { Avatar, AvatarFallback, AvatarImage } from "./ui/avatar";
 import { setSelectedUser } from "../redux/authSlice";
+import { Input } from "./ui/input";
+import { Button } from "./ui/button";
+import Messages from "./Messages";
 const Chat = () => {
   const { user, selectedUser, suggested } = useSelector((store) => store.auth);
   const status = true;
@@ -50,7 +53,7 @@ const Chat = () => {
         </div>
         <div className="w-3/4 overflow-y-auto h-full border border-white">
           {selectedUser ? (
-            <div className="relative">
+            <div className="relative flex flex-col h-full">
               <div className=" sticky top-0 bg-white dark:bg-gray-800 px-1 py-2 flex gap-1 items-center">
                 <Avatar className="size-10">
                   <AvatarImage
@@ -61,6 +64,11 @@ const Chat = () => {
                   <AvatarFallback>CN</AvatarFallback>
                 </Avatar>
                 <div>{selectedUser.username}</div>
+              </div>
+              <Messages selectedUser={selectedUser}/>
+              <div className="flex sticky bottom-0 items-center p-2">
+                <Input type="text" placeholder="Type Message..." className="focus-visible:ring-transparent flex-1 mr-1" />
+                <Button>Send</Button>
               </div>
             </div>
           ) : (
---
+++ frontend/src/components/Messages.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Messages = () => {
+  return (
+    <div className='flex-1 overflow-y-auto border p-2 border-yellow-200'>
+hi
+    </div>
+  )
+}
+
+export default Messages
