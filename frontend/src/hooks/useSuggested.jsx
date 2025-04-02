9b4f79a7b69655eb13969ec383510f774784d591
sahoonishi <nishisahoo293@gmail.com>
2025-04-02 19:54:26 +0530

    suggested api remove

--- frontend/src/hooks/useSuggested.jsx
+++ frontend/src/hooks/useSuggested.jsx
@@ -11,7 +11,7 @@ const useSuggested = () => {
       
       try {
         const res = await axios.get(
-          `${import.meta.env.VITE_API_URL}/api/user/suggested`,
+          `${import.meta.env.VITE_API_URL}/user/suggested`,
           { withCredentials: true }
         );
         if (res.data.success) {
